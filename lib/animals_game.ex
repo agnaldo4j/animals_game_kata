6031442c28a8f01bb5757e4d71475d9a0122fe31
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2015-09-27 21:22:51 -0300

    versão final

--- lib/animals_game.ex
+++ lib/animals_game.ex
@@ -7,33 +7,33 @@ defmodule AnimalsGame do
 
 		IO.gets Question.readyQuestion()
 
-		_question(Question.firstQuestion())
+		_questionLooper(Question.firstQuestion())
+	end
+
+	defp _questionLooper(question = %Question{}) do
+		response = IO.gets question.message
+		if (response == "sim\n") do 
+			newState = %{question | yes: _question(question.yes)}
+		else
+			newState = %{question | no: _question(question.no)}
+		end
+		_questionLooper(newState)
 	end
 
 	defp _question(question = %Question{}) do
 		response = IO.gets question.message
 		if (response == "sim\n") do 
-			newState = %{question | yes: _question(question.yes, question)}
-		else
-			newState = %{question | no: _question(question.no, question)}
-		end
-		_question(newState)
-	end
-
-	defp _question(question = %Question{}, state = %Question{}) do
-		response = IO.gets question.message
-		if (response == "sim\n") do 
-			%{question | yes: _question(question.yes, question)}
+			%{question | yes: _question(question.yes)}
 		else
 			if(question.no == %Learn{}) do
 				_question(question.no, question)
 			else
-				%{question | no: _question(question.no, question)}
+				%{question | no: _question(question.no)}
 			end
 		end
 	end
 
-	defp _question(victory = %Victory{}, state = %Question{}) do
+	defp _question(victory = %Victory{}) do
 		IO.puts victory.message
 		response = IO.gets victory.playAgain
 		if (response == "sim\n") do 
